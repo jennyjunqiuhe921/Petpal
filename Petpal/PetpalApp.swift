4c54cf113bb5247a20f9b4e136b11e8ded048973
Jennyhe <jennyjunqiuhe921@gmail.com>
2026-01-07 14:07:58 +0800

    Initial Commit

---
+++ Petpal/PetpalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetpalApp.swift
+//  Petpal
+//
+//  Created by gong on 2026/1/7.
+//
+
+import SwiftUI
+
+@main
+struct PetpalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
